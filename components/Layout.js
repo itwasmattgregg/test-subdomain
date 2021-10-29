3f1b4187b38915c4907030150d1751b456beaa22
Matt Gregg <mattdgregg@gmail.com>
2021-10-29 09:39:24 -0500

    Styles

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,9 @@ import styles from '../styles/Layout.module.css';
 export default function Layout({ children }) {
   return (
     <>
-      <div className={styles.navigation}></div>
-      <h1>Navigation area</h1>
+      <div className={styles.navigation}>
+        <h1>Navigation area</h1>
+      </div>
       {children}
     </>
   );
